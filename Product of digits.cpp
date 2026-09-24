a23934787cc6da7dfa4b1669b4d2bcfe8c6d7527
Tubaislam123 <si3536305@gmail.com>
2026-09-24 21:58:03 +0600

    message

---
+++ Product of digits.cpp
@@ -0,0 +1,19 @@
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n,p=1;
+    cin>>n;
+
+    while(n!=0)
+    {
+        p=p*(n%10);
+        n=n/10;
+    }
+
+    cout<<p;
+
+    return 0;
+}
---
+++ Reverse a number.cpp
@@ -0,0 +1,19 @@
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n,r=0;
+    cin>>n;
+
+    while(n!=0)
+    {
+        r=r*10+n%10;
+        n=n/10;
+    }
+
+    cout<<r;
+
+    return 0;
+}
